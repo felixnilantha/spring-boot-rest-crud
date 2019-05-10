2e06e5d3df9c59e7b177991c61d6996ced25c86d
Nilantha Silva <felixnilantha@gmail.com>
2019-05-10 12:26:25 +1000

    example with @RepositoryRestResource

--- 04.spring-boot-rest-spring-data-rest/src/main/java/com/spring/boot/rest/crud/repository/EmployeRepository.java
+++ 04.spring-boot-rest-spring-data-rest/src/main/java/com/spring/boot/rest/crud/repository/EmployeRepository.java
@@ -1,10 +1,12 @@
 package com.spring.boot.rest.crud.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.spring.boot.rest.crud.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeRepository extends JpaRepository<Employee, Integer> {
-	
 
+	
 }
